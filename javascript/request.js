3e741cb916c91f68999c44b4e3ffeee3e5e62517
AnaCarolinaPGDO <anagross60@gmail.com>
2024-09-26 19:59:06 -0300

    Muds

---
+++ javascript/request.js
@@ -0,0 +1,32 @@
+
+
+
+
+
+
+
+
+const base_url = "https://66f5d156436827ced97536d3.mockapi.io/api/v1/:endpoint"
+
+const function cadastro() {
+    let nome = document.getElementById("nome").value;
+    let email = document.getElementById("email").value;
+    let senha = document.getElementById("senha").value;
+
+    const user = {
+        name: name,
+        email: email,
+    }
+
+    fetch(base_url, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+
+        body: JSON.stringify(user)
+    })
+    .then(resposta => resposta.json())
+    .then(resultado => alerrt('Usuario criado com sucesso!'))
+    .catch(error => console.log(error))
+}
