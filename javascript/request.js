1536cdc88ef4ceb80a2f76c32b4c0ffc9f7fa157
eric.s <ericcarvalhosilva03@gmail.com>
2024-10-01 17:15:39 -0300

    arrumando css de cadastro e fazendo funcionar o cadastro

--- javascript/request.js
+++ javascript/request.js
@@ -1,23 +1,23 @@
 
-function cadastro() {
-    let nome = document.getElementById("nome").value;
-    let email = document.getElementById("email").value;
-    let senha = document.getElementById("senha").value;
+// function cadastro() {
+//     let nome = document.getElementById("nome").value;
+//     let email = document.getElementById("email").value;
+//     let senha = document.getElementById("senha").value;
 
-    const user = {
-        name: nome,//+
-        email: email,
-    }
+//     const user = {
+//         name: nome,//+
+//         email: email,
+//     }
 
-    fetch(base_url, {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json'
-        },
+//     fetch(base_url, {
+//         method: 'POST',
+//         headers: {
+//             'Content-Type': 'application/json'
+//         },
 
-        body: JSON.stringify(user)
-    })
-    .then(resposta => resposta.json())
-    .then(resultado => alert('Informação inválida!'))
-    .catch(error => console.log(error))
-}
+//         body: JSON.stringify(user)
+//     })
+//     .then(resposta => resposta.json())
+//     .then(resultado => alert('Informação inválida!'))
+//     .catch(error => console.log(error))
+// }
